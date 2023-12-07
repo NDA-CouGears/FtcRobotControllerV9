13e792aeddfc86b44cb58043290ed0d53ea0ea87
Bill Clogston <bill@clogstons.com>
2023-12-07 16:11:56 -0500

    Adding red version of near and far auto modes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedPropFar.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+
+@Autonomous(name = "Red Team Prop Far 1", group = "Team OpModes")
+public class RedPropFar extends AutoMode {
+    protected TfodProcessor getProcessor(){
+        String[] labels = {"Blue Prop"};
+        TfodProcessor newProcessor = new TfodProcessor.Builder()
+                .setModelAssetName("redtrainingfinal.tflite")
+                .setModelLabels(labels)
+                .setIsModelQuantized(true)
+                .build();
+        return newProcessor;
+    }
+
+    @Override
+    protected boolean isBlue(){
+        return true;
+    }
+
+    @Override
+    protected boolean isNear() {
+        return false;
+    }
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedPropNear.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+
+@Autonomous(name = "Red Team Prop Near 1", group = "Team OpModes")
+public class RedPropNear extends AutoMode {
+    protected TfodProcessor getProcessor(){
+        String[] labels = {"Blue Prop"};
+        TfodProcessor newProcessor = new TfodProcessor.Builder()
+                .setModelAssetName("redtrainingfinal.tflite")
+                .setModelLabels(labels)
+                .setIsModelQuantized(true)
+                .build();
+        return newProcessor;
+    }
+
+    @Override
+    protected boolean isBlue(){
+        return false;
+    }
+
+    @Override
+    protected boolean isNear() {
+        return true;
+    }
+}
+
